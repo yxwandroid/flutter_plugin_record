b6cdfa3ecb97fdadb2acc6e5607fd6a2fbb68996
wilson <yxw_android@163.com>
2020-05-16 10:47:22 +0800

    更新代码

--- lib/index.dart
+++ lib/index.dart
@@ -1,2 +1,3 @@
 export 'voice_widget.dart';
 export 'flutter_plugin_record.dart';
+export 'record_state.dart';
---
+++ lib/record_state.dart
@@ -0,0 +1,17 @@
+class RecordState {
+  ///发送到原生端的方法名
+  static String init = "init";
+  static String start = "start";
+  static String startByWavPath = "startByWavPath";
+  static String stop = "stop";
+  static String play = "play";
+  static String playByPath = "playByPath";
+
+  ///原生端的回调方法名
+  static String onInit = "onInit";
+  static String onStart = "onStart";
+  static String onStop = "onStop";
+  static String onPlay = "onPlay";
+  static String onAmplitude = "onAmplitude";
+  static String onPlayState = "onPlayState";
+}
